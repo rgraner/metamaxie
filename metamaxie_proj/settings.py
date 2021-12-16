6b54c1608e76792f896e3a60bbaca913d7ea046f
rgraner <ricardograner@gmail.com>
2021-12-16 20:33:17 +0000

    Set DEBUG to false.

--- metamaxie_proj/settings.py
+++ metamaxie_proj/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-k#%+r%ur232y6axxjg$#vo!68hxe5_n$i@tq8&v4=!vrgt9$vl'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 # Application definition
 
