3e57f219839d7bd7fdd2c1b31bf837a001b3aaa3
rgraner <ricardograner@gmail.com>
2021-12-17 16:36:44 +0000

    Fixing static files in settings and urls, revert media folder to root.

--- metamaxie_proj/settings.py
+++ metamaxie_proj/settings.py
@@ -136,23 +136,22 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR/'static']
-STATIC_ROOT = BASE_DIR/'staticfiles'
-
-MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR/'static/media'
-
 # STATIC_URL = '/static/'
+# STATICFILES_DIRS = [BASE_DIR/'static']
+# STATIC_ROOT = BASE_DIR/'staticfiles'
+
 # MEDIA_URL = '/media/'
-
-# if DEBUG:
-#   STATICFILES_DIRS = [BASE_DIR/'static']
-# else:
-
-#   STATIC_ROOT = BASE_DIR/'staticfiles'
-
 # MEDIA_ROOT = BASE_DIR/'media'
+
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+if DEBUG:
+  STATICFILES_DIRS = [BASE_DIR/'static']
+else:
+  STATIC_ROOT = BASE_DIR/'staticfiles'
+
+MEDIA_ROOT = BASE_DIR/'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- metamaxie_proj/urls.py
+++ metamaxie_proj/urls.py
@@ -18,8 +18,14 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+from django.conf.urls import url
+from django.views.static import serve
+
 
 urlpatterns = [
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
     path('admin/', admin.site.urls),
     path('users/', include('users.urls', namespace='users')),
     path('users/', include('django.contrib.auth.urls')),
@@ -29,8 +35,8 @@ urlpatterns = [
     path('tasks/', include('tasks.urls', namespace='tasks')), 
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 admin.site.site_header = "Metamaxie Administration"
