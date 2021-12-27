8c1a645258710a5030992a661fefff8a1704a218
rgraner <ricardograner@gmail.com>
2021-12-27 16:36:37 +0000

    Fix previous condition.

--- payments/views.py
+++ payments/views.py
@@ -47,7 +47,7 @@ def payment_view(request):
         df_payment['built_up_slp_shift'] = df_payment.groupby('scholarship')['built_up_slp'].shift(1)
         df_payment['last_claim_amount'] = df_payment['built_up_slp'] - df_payment['built_up_slp_shift']
 
-        df_payment.loc[(df_payment['last_claim_amount']<0), 'last_claim_amount'] = df_payment['last_claim_amount'] * -1
+        df_payment.loc[(df_payment['last_claim_amount']<0)] = df_payment['last_claim_amount'] * -1
 
 
         df_payment['last_claim_shift'] = df_payment.groupby('scholarship')['last_claim'].shift(1)
