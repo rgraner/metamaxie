d552771678e76750ea77395a413be708179ba7f8
rgraner <ricardograner@gmail.com>
2022-01-05 14:59:24 +0000

    Fix avatar uploaded.

--- users/admin.py
+++ users/admin.py
@@ -5,28 +5,35 @@ from .forms import RegistrationForm
 from django.contrib.auth.admin import UserAdmin
 
 
-class CustomUserAdmin(UserAdmin):
-    model = User
-    add_form = RegistrationForm
+# class CustomUserAdmin(UserAdmin):
+#     model = User
+#     add_form = RegistrationForm
+#     list_display = ('username', 'type')
+
+#     fieldsets = (
+#         *UserAdmin.fieldsets,
+#         (
+#             'use role',
+#             {
+#                 'fields': (
+#                     'type',
+#                     'avatar',
+#                 )
+            
+#             }
+#         )
+
+#     )
+
+class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'type')
 
-    fieldsets = (
-        *UserAdmin.fieldsets,
-        (
-            'use role',
-            {
-                'fields': (
-                    'type',
-                    'avatar',
-                )
-            
-            }
-        )
+class ManagerTeamAdmin(admin.ModelAdmin):
+    list_display = ('manager', 'team_code')
 
-    )
 
-admin.site.register(User, CustomUserAdmin)
+admin.site.register(User, UserAdmin)
 admin.site.register(Manager)
-admin.site.register(ManagerTeam)
+admin.site.register(ManagerTeam, ManagerTeamAdmin)
 admin.site.register(Scholar)
 admin.site.register(ScholarTeam)
