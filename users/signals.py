496cb8b4d634f1fe6f4e631f63a985e661161b44
rgraner <ricardograner@gmail.com>
2022-01-07 19:26:34 +0000

    Remove folder and files from media folder related to user when the user is deleted.

--- users/signals.py
+++ users/signals.py
@@ -2,8 +2,10 @@ from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
 
 from .models import User, Manager, ScholarTeam, ManagerTeam
+from metamaxie_proj.settings import MEDIA_ROOT
 
-#import os
+import os
+import shutil
 
 
 # signal for custom authentication
@@ -16,6 +18,19 @@ def post_save_create_scholarship_payment(sender, instance, created, **kwargs):
             ManagerTeam.objects.create(manager=instance)
         else:
             ScholarTeam.objects.create(scholar=instance)
+
+
+@receiver(post_delete, sender=User)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    profile_avatar_name = '{}_{}_{}'.format(instance.type, instance.username, instance.id)
+    path = os.path.join(MEDIA_ROOT, profile_avatar_name)
+    """
+    Deletes file from filesystem
+    when corresponding `MediaFile` object is deleted.
+    """
+    if instance.avatar:
+        if os.path.isdir(path):
+            shutil.rmtree(path, ignore_errors=False, onerror=None)
 
 
 # @receiver(post_delete, sender=User)
