903865441ab3f7da0d1cdc98891ad04347598200
rgraner <ricardograner@gmail.com>
2022-03-13 16:56:53 +0000

    Updated smtp credentials

--- metamaxie_proj/settings.py
+++ metamaxie_proj/settings.py
@@ -169,8 +169,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'ricardograner@gmail.com'
-EMAIL_HOST_PASSWORD = 'clubedaesquina'
+EMAIL_HOST_USER = 'metamaxie21@gmail.com'
+EMAIL_HOST_PASSWORD = 'jk\sdhfgudk@utf'
 
 
 # Heroku settings.
