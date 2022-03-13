60d0dc3460be44e921bd5f02868ae4dfef83f3e5
rgraner <ricardograner@gmail.com>
2022-03-13 16:50:15 +0000

    Updated smtp credentials

--- metamaxie_proj/settings.py
+++ metamaxie_proj/settings.py
@@ -169,9 +169,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'metamaxie21@gmail.com'
-EMAIL_HOST_PASSWORD = 'jk\sdhfgudk@utf'
-DEFAULT_FROM_EMAIL = 'default from email'
+EMAIL_HOST_USER = 'ricardograner@gmail.com'
+EMAIL_HOST_PASSWORD = 'clubedaesquina'
 
 
 # Heroku settings.
